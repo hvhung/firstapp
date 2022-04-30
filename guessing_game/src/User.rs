9a676c98d15ee0fef26b4fe47cd05d2cb0c4ceb5
hvhung <hunghuynhvan@live.com>
2022-04-30 22:39:11 +0700

    sleep time

---
+++ guessing_game/src/User.rs
@@ -0,0 +1,6 @@
+pub struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+// use sample_primitive;
+
 fn main() {
     println!("Hello, world!");
+    print_sample_primitive(true);
+}
+
+fn print_sample_primitive(show_sample: bool) {
+    if show_sample {
+        // TODO: import another file and use
+    }
 }
